45fb7d63e37801ce262e741d32ba89d03613222d
null62 <mariamsamaha6@gmail.com>
2025-05-08 22:05:14 +0300

    Create Streaming.js

---
+++ backend/src/Streaming.js
@@ -0,0 +1,33 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  socket.on("room:join", (bookId) => {
+    socket.join(bookId);
+    console.log(`User ${socket.id} joined ${bookId}`);
+
+    const users = io.sockets.adapter.rooms.get(bookId);
+    if (users.size > 1) {
+      const streamerId = [...users][0];
+
+      io.to(streamerId).emit("room:listener-joined", socket.id);
+      io.to(socket.id).emit("room:streamer-connected", streamerId);
+    }
+  });
+  
+  socket.on("peer:signal", (targetId, data) => {
+    socket.to(targetId).emit("peer:signal", socket.id, data);
+    console.log(`Signal from ${socket.id} to ${targetId}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected ${socket.id}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
