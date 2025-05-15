2bb9c3690d17a45dc898908f02122bfcf1102f82
MME517 <125832571+MME517@users.noreply.github.com>
2025-05-15 06:26:04 +0300

    streaming backend server

---
+++ streaming-server/server.js
@@ -0,0 +1,33 @@
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("room:join", (streamId) => {
+    socket.join(streamId);
+    console.log(`User ${socket.id} joined stream ${streamId}`);
+
+    const users = io.sockets.adapter.rooms.get(streamId);
+    if (users && users.size > 1) {
+      const streamerId = [...users][0]; // First user in the room is the streamer
+      io.to(streamerId).emit("room:listener-joined", socket.id);
+      io.to(socket.id).emit("room:streamer-connected", streamerId);
+    }
+  });
+
+  socket.on("peer:signal", (targetId, data) => {
+    socket.to(targetId).emit("peer:signal", socket.id, data);
+    console.log(`Signal from ${socket.id} to ${targetId}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+console.log("Socket.IO server running on port 3001");
